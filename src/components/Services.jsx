b9b47acb38eab4cb5d95fa1645727ae17ced7ed5
unknown <shanusharma5005@gmail.com>
2025-03-18 23:46:25 +0530

    adding normal css property

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -6,7 +6,7 @@ const Services = () => {
    
     <div className='py-20 bg-gray-100 border-b-2  '>
 <h2 className='text-center text-3xl font-bold'>Our Services</h2>
-<div className='flex mt-10 gap-6 w-full justify-evenly'>
+<div className='flex mt-10 w-full justify-evenly'>
 
     <div  className='bg-slate-100 border-2 p-6 rounded-lg '>
 <h3 className='text-lg font-bold'>Web development</h3>
@@ -19,12 +19,7 @@ const Services = () => {
 <p className='text-gray-800 mt-2'>rank higher in search engine </p>
 
         </div>
-
-        <div  className='bg-slate-100 border-2 p-6 rounded-lg  '>
-<h3 className='text-lg font-bold'>Marketing </h3>
-<p className='text-gray-800 mt-2'>grow your audience efficiently </p>
-
-        </div>
+        
 
 </div>
 
